ad8a8bd776a62bd873bfbc28de35dd3fbc74b369
Tahar <taharbelhouchat@hotmail.com>
2022-04-26 05:41:52 +0100

    Fix some errors

--- get-list.js
+++ get-list.js
@@ -1,53 +1,73 @@
 #!/usr/bin/env node
+
 /**
  * Grab the list of protocol numbers from iana
  * and spit out a javascript file to use
  */
+const axios = require("axios");
+const parseString = require("xml2js").parseString;
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const LocalizedFormat = require("dayjs/plugin/localizedFormat");
 
-var request = require('request');
-var xml2js = require('xml2js');
-var url = 'http://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml';
-
-var parser = new xml2js.Parser();
-var ret = {};
-
-// Get the URL
-request(url, function(err, res, body) {
-  if (err) throw err;
-  if (res.statusCode !== 200)
-    throw new Error('Status code: ' + res.statusCode);
-
-  // Parse some XML
-  parser.parseString(body, function(err, obj) {
-    if (err) throw err;
-
-    // starting point of the protocols
-    var base = obj.registry.record;
-
-    base.forEach(function(proto) {
-      clean_obj(proto);
-
-      if (!ret[proto.protocol]) ret[proto.protocol] = {};
-      ret[proto.protocol][proto.number] = {
-        name: proto.name,
-        description: proto.description
-      };
-    });
-
-    // Make the js output and print it
-    s = [
-      'var services = ' + JSON.stringify(ret, null, 2) + ';',
-      'if (module && module.exports) module.exports = services;'
-    ].join('\n');
-    console.log(s);
-  });
-});
+const serviceNames = {};
 
 /**
  * Clean up arbitrary arrays in an obj
  */
-function clean_obj(obj) {
-  Object.keys(obj).forEach(function(key) {
-    if (obj[key].length) obj[key] = obj[key][0];
+const cleanRecord = (record) => {
+  Object.keys(record).forEach((key) => {
+    if (record[key].length) record[key] = record[key][0];
   });
-}
+};
+
+// Get the URL
+const url =
+  "https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml";
+const iana = axios.create({
+  method: "get",
+  baseURL: url,
+  timout: 300000, // 5 minutes
+});
+
+// Get the date of the last update
+dayjs.extend(utc);
+dayjs.extend(LocalizedFormat);
+const lastUpdate = dayjs().utc().format("L");
+
+// GET the list of protocols
+(async () => {
+  try {
+    const { data } = await iana(); // Get the data
+
+    // Parse some XML
+    parseString(data, (error, result) => {
+      if (error) throw error;
+
+      // starting point of the protocols
+      const records = result.registry.record;
+
+      records.forEach((record) => {
+        cleanRecord(record);
+
+        if (!serviceNames[record.protocol]) serviceNames[record.protocol] = {};
+        serviceNames[record.protocol][record.number] = {
+          name: record.name ? record.name : "",
+          description: record.description ? record.description : "",
+        };
+      });
+
+      // Make the js output and print it
+      output = [
+        `// Last update: ${lastUpdate}`,
+        "const services = ",
+        JSON.stringify(serviceNames, null, 2),
+        ";",
+        "module.exports = services",
+      ].join("\n");
+      console.log(output);
+    });
+  } catch (error) {
+    throw error;
+  }
+})();
